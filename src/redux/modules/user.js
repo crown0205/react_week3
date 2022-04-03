f3af16f5aa9da148211a3ab85ea0b5cb5b306da7
crown0205 <crown0205@gmail.com>
2022-04-04 02:24:07 +0900

    위치 바뀌면 랜더링시 에러남!! 주의하자~!!

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -2,15 +2,15 @@ import produce from "immer";
 import { createAction, handleActions } from "redux-actions";
 import { setCookie } from "../../shared/Cookie";
 
-// action creators // 액션함수를 "redux-actions"이 패키지로 받아 편하게 바꿔준거다.
-const logIn = createAction(LOG_IN, user => ({ user }));
-const logOut = createAction(LOG_OUT, user => ({ user }));
-const getUser = createAction(GET_USER, user => ({ user }));
-
 //actions type
 const LOG_IN = "LOG_IN";
 const LOG_OUT = "LOG_OUT";
 const GET_USER = "GET_USER";
+
+// action creators // 액션함수를 "redux-actions"이 패키지로 받아 편하게 바꿔준거다.
+const logIn = createAction(LOG_IN, user => ({ user }));
+const logOut = createAction(LOG_OUT, user => ({ user }));
+const getUser = createAction(GET_USER, user => ({ user }));
 
 // initialState
 const initialState = {
